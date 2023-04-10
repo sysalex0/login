18ff78f609165388bc0cb79ff7d938fcfbdfe6b2
Alex Shiu <sysalex0@gmail.com>
2023-04-11 00:06:39 +0800

    - make sure db is connected by adding a model - update docker-compose.yml for later kustomize transformation

--- src/main/java/penguin/login/model/User.java
+++ src/main/java/penguin/login/model/User.java
@@ -9,7 +9,8 @@ import lombok.*;
 @Getter
 @Setter
 @ToString
-@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
